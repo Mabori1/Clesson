76dc31283f7eb807f64fc506633ea130f1b1bc43
Alexey <makeevhome@mail.ru>
2022-08-26 02:22:26 +0300

    add Task23

---
+++ Task23/Task23.cs
@@ -0,0 +1,23 @@
+﻿
+//Задача 23
+//Напишите программу, которая принимает на вход число (N)
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+class MyClass
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Введите число: ");
+		
+		int n = int.Parse(Console.ReadLine());
+
+		Pow3(n);
+	}
+
+	private static void Pow3(int n)
+	{
+		for (int i = 1; i <= n; i++)
+		
+			Console.Write(Math.Pow(i, 3) + " ");		
+	}
+}
