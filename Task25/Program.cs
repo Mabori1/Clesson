f236535617a9b9ebc98ad8a57a7f272615ace16e
Alexey <makeevhome@mail.ru>
2022-08-29 22:50:25 +0300

    add task25

---
+++ Task25/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+
+class Myclass
+{
+
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите 2 числа через пробел: ");
+
+        string[] str = Console.ReadLine().Split(" ");
+        int a = Convert.ToInt32(str[0]);
+        int b = Convert.ToInt32(str[1]);
+
+        Console.WriteLine($"Число {a} в степени {b} = {myPow(a, b)}");
+        Console.ReadLine();
+    }
+
+
+
+    static double myPow(int a, int b)
+    {
+        return Math.Pow(a, b);
+    }
+}
