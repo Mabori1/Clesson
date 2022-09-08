522ee9958c5b3206f165777da306e05827ae070c
Alexey <makeevhome@mail.ru>
2022-09-09 01:06:13 +0300

    add 47 52

--- Task49/Program.cs
+++ Task49/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 49: Задайте двумерный массив. Найдите элементы, 
+﻿ //Задача 49: Задайте двумерный массив. Найдите элементы,
 // у которых оба индекса чётные, и замените эти элементы на их квадраты.
 // Например, изначально массив выглядел вот так:
 // 1 4 7 2
@@ -8,32 +8,57 @@
 // 1 4 7 2
 // 5 81 2 9
 // 8 4 2 4
-
-
-void createArray(int a, int b) 
+ 
+ 
+int[,] CreateAndFullArray(int a, int b)
 {
-    int[,] array = new int[a,b];
-
-    for (int m = 0; m < array.GetLength(0); m++)
-    {
-        System.Console.WriteLine();
-
-        for (var n = 0; n < array.GetLength(1); n++)
-        {
-            if((n != 0) && (m != 0) && (m % 2 == 0) && (n % 2 == 0) )   
-            {
-                array[m,n] = (int)Math.Pow(new Random().Next(2,3), 2);            
-            }
-
-            else {
-             array[m,n] = new Random().Next(2,3);     
-            }
-            System.Console.Write(array[m,n]);
-            System.Console.Write(" ");
-
-        }
-        
-    }   
+   int[,] array = new int[a,b];
+ 
+   for (int m = 0; m < array.GetLength(0); m++)
+   {
+       //System.Console.WriteLine();
+ 
+       for (var n = 0; n < array.GetLength(1); n++)
+       {
+           array[m,n] = new Random().Next(2,3);
+       }
+   } 
+   return  array;
 }
-
-createArray(6,6);
+ 
+void PowChetIndex(int[,] array)
+{
+   for (int m = 0; m < array.GetLength(0); m++)
+   {
+       //System.Console.WriteLine();
+ 
+       for (var n = 0; n < array.GetLength(1); n++)
+       {
+           bool notNullIndex = (n != 0) && (m != 0);
+           if(notNullIndex && (m % 2 == 0) && (n % 2 == 0)) 
+           {
+               array[m,n] = (int)Math.Pow(array[m,n], 2);          
+           }
+       }
+   }
+}
+ 
+void PrintArray(int[,] array)
+{
+   for (int m = 0; m < array.GetLength(0); m++)
+   {
+ 
+       for (var n = 0; n < array.GetLength(1); n++)
+       {
+           System.Console.Write(array[m,n]);
+           System.Console.Write(" ");
+       }
+       System.Console.WriteLine();
+   }
+}
+ 
+int[,] array = CreateAndFullArray(8,8);
+PrintArray(array);
+System.Console.WriteLine();
+PowChetIndex(array);
+PrintArray(array);
---
+++ Task50/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+// Задача 50. Напишите программу, которая на вход принимает
+// позиции элемента в двумерном массиве, и возвращает значение 
+// этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+int[,] createArray(int a, int b) 
+{
+    int[,] array = new int[a,b];
+
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        System.Console.WriteLine();
+
+        for (var n = 0; n < array.GetLength(1); n++)
+        {
+            array[m,n] = new Random().Next(0,10);
+            System.Console.Write(array[m,n] + " ");
+        }
+        
+    }   
+    return array;
+}
+
+createArray(3,4);
+
+void findInArray( int[,] arr, int findNum)
+{
+    for (int m = 0; m < arr.GetLength(0); m++)
+    {
+        for (var n = 0; n < arr.GetLength(1); n++)
+        {
+            if(findNum == arr[m,n])
+            System.Console.WriteLine($"Найдено число: {arr[m,n]} ");
+            else
+            System.Console.WriteLine("Такого числа нет.");
+        }
+    }
+}
+
+findInArray( createArray(4,4), 12);
