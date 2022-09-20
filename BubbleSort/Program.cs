357c2968bd4e95345ebc840f8e5d855610c001bd
Alexey Makeev <makeevhome@mail.ru>
2022-09-21 01:19:02 +0300

    2

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,59 @@
+﻿// C # программа для реализации
+// пузырьковой сортировки
+
+using System;
+
+
+
+class GFG
+
+{
+    static void bubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        bool swapped = false;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++)
+            {
+
+                if (arr[j] > arr[j + 1])
+                {
+                    // поменяйте местами temp и arr [i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+
+    static void printArray(int[] arr)
+
+    {
+        for (int i = 0; i < arr.Length; ++i)
+            Console.Write(arr[i] + " ");
+        Console.WriteLine();
+    }
+
+    public static void Main()
+
+    {
+
+        int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+
+        bubbleSort(arr);
+
+        Console.WriteLine("Sorted array");
+
+        printArray(arr);
+
+    }
+
+
+}
--- Test2/Program.cs
+++ Test2/Program.cs
@@ -5,64 +5,71 @@
 
 using System;
 
-class GFG {  
 
-static void thirdLargest(int[] arr) 
+class GFG
 {
 
-    / * Должно быть не менее трех элементов * /
-
-    if (arr.Length < 3)
-    {
-        Console.Write(" Invalid Input ");
-        return;
-    }  
-
-    // Инициализируем первый, второй и третий самый большой элемент
-
-    int first = arr[0], 
-        second = int.MinValue,
-        third = int.MinValue;  
-
-    // Обходим элементы массива, чтобы найти третий по величине
-
-    for (int i = 1; i < arr_size; i++) 
+    static void thirdLargest(int[] arr)
     {
 
-        / * Если текущий элемент больше первого,
 
-        затем обновите первый, второй и третий * /
-
-        if (arr[i] > first) {
-
-            third = second;
-            second = first;
-            first = arr[i];
-        }          
-
-        / * Если arr [i] находится между первым и вторым * /
-
-        else if (arr[i] > second) {
-
-            third = second;
-            second = arr[i];
-        } 
-
-        / * Если arr [i] находится между вторым и третьим * /
-
-        else if (arr[i] > third) {
-
-            third = arr[i];
+        if (arr.Length < 3)
+        {
+            Console.Write(" Invalid Input ");
+            return;
         }
-    }
-    Console.Write("The third Largest element is "+ third);
-}
-    
-public static void Main() {
 
-        int[] arr = {12, 13, 1, 10, 34, 16};        
+        // Инициализируем первый, второй и третий самый большой элемент
+
+        int first = arr[0],
+            second = int.MinValue,
+            third = int.MinValue;
+
+        // Обходим элементы массива, чтобы найти третий по величине
+
+        for (int i = 1; i < arr.Length; i++)
+        {
+
+            /* Если текущий элемент больше первого,
+
+            затем обновите первый, второй и третий */
+
+
+            if (arr[i] > first)
+            {
+
+                third = second;
+                second = first;
+                first = arr[i];
+            }
+
+            /* Если arr[i] находится между первым и вторым */
+
+            else if (arr[i] > second && arr[i] != first)
+            {
+
+                third = second;
+                second = arr[i];
+            }
+
+            /* Если arr[i] находится между вторым и третьим */
+
+            else if (arr[i] > third && arr[i] != second)
+            {
+                third = arr[i];
+            }
+        }
+        Console.Write($"Первый элемент: {first}\n" +
+                      $"Второй элемент: {second}\n" +
+                      $"Третий элемент: {third}");
+    }
+
+    public static void Main()
+    {
+
+        // int[] arr = { 12, 13, 1, 10, 34, 16 };
+        int[] arr = { 12, 13, 13, 1, 10, 34, 34, 16, 16, };
 
         thirdLargest(arr);
-
     }
 }
